fe28dd5e4d670d65d03f4928a41775c9f0a2a6f3
raul-dunca <lol.momenta2657@gmail.com>
2025-10-29 10:31:26 +0100

    first commit praydge

---
+++ web_app.py
@@ -0,0 +1,35 @@
+import os
+from flask import Flask, Response
+import psycopg2
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    host=os.getenv('DB_HOST')
+    dbname=os.getenv('DB_NAME')
+    user=os.getenv('DB_USER')
+    password=os.getenv('DB_PASS')
+
+    conn = psycopg2.connect(host=host, dbname=dbname, user=user, password=password,options="-c search_path=public")
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM cs2_skins;")
+
+    rows = cur.fetchall()
+
+    # Get column names
+    colnames = [desc[0] for desc in cur.description]
+
+    # Filter out 'id' column
+    filtered_cols = [i for i, col in enumerate(colnames) if col != 'id']
+
+    # Format each row as a string without the id
+    lines = [" | ".join(f"{colnames[i]}: {row[i]}" for i in filtered_cols) for row in rows]
+
+    cur.close()
+    conn.close()
+
+    return Response("\n".join(lines), mimetype='text/plain')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=80)
